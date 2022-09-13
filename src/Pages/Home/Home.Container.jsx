a71e674be54216729d6d7338bb9ee6f36c5c6e65
Ankit Gandhi <ankitgandhi452@gmail.com>
2022-09-13 15:32:07 +0530

    Consoles removed

--- src/Pages/Home/Home.Container.jsx
+++ src/Pages/Home/Home.Container.jsx
@@ -3,7 +3,6 @@ import HomeWrapper from './Home.Wrapper'
 
 export default class HomeContainer extends PureComponent {
   render () {
-    console.log(this.props)
     return (
       <HomeWrapper />
     )
