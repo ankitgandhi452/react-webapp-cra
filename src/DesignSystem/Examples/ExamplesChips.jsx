f71babfa5d01a65862b870bd39a9c6bc85b77fed
Mehul Agarwal <mehulagarwal@Mehuls-MacBook-Pro.local>
2022-11-04 15:24:38 +0530

    Add Chips Example Page

---
+++ src/DesignSystem/Examples/ExamplesChips.jsx
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { Grid, Chip } from '@mui/material'
+
+class ExamplesChips extends PureComponent {
+  render() {
+    return (
+      <Grid container spacing={2} />
+    )
+  }
+}
+
+export default ExamplesChips
+
+ExamplesChips.propTypes = {
+}
+
+ExamplesChips.defaultProps = {
+}
